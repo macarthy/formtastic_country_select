79b51a582f22418801b138bdc7f5e0aca6c3d19b
Justin MacCarthy <justin@rubystars.biz>
2014-06-23 14:01:51 +0700

    Basic rspec setup

---
+++ lib/formtastic_country_select.rb
@@ -0,0 +1,5 @@
+require "formtastic_country_select/version"
+
+module FormtasticCountrySelect
+  # Your code goes here...
+end
---
+++ lib/formtastic_country_select/version.rb
@@ -0,0 +1,3 @@
+module FormtasticCountrySelect
+  VERSION = "0.0.1"
+end
